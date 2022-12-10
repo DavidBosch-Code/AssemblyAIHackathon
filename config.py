dccf3fe658798d4dd66ceaa045ec7b924039f1d1
Nathan Bosch <nathangerbenbosch@gmail.com>
2022-12-10 21:12:47 +0100

    Add OpenAI calls to generate summaries & takeaways from interviews

--- config.py
+++ config.py
@@ -5,20 +5,21 @@ load_dotenv()
 
 
 class ApplicationConfig():
-    API_KEY = os.environ["API_KEY"]
+    API_KEY = os.environ["ASSEMBLY_API_KEY"]
     SQLALCHELY_ECHO = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///project.db"
     CORS_HEADERS = "Content-Type"
 
 
-headers = {}  # place hearders for assembly AI here
+headers = {}  # place headers for assembly AI here
 
 ASSEMBLY_URL = "https://api.assemblyai.com/v2/"
 
 ASSEMBLY_UPLOAD_ENDPOINT = ASSEMBLY_URL + 'upload'
 ASSEMBLY_TRANSCRIPT_ENDPOINT = ASSEMBLY_URL + 'transcript'
 
-API_KEY = os.environ["API_KEY"]
+ASSEMBLY_API_KEY = os.environ["ASSEMBLY_API_KEY"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 CACHE_FOLDER = os.path.join(os.getcwd(), 'cache')
 CSV_CACHE = os.path.join(CACHE_FOLDER, 'cache.csv')
